9e53dfc0ecd62082e66dbd376653eb30dfceb9c8
kepsgurih <kepsgurih@pm.me>
2021-03-13 12:08:11 +0700

    Adding CSS

--- application/views/login.php
+++ application/views/login.php
@@ -2,65 +2,7 @@
 <html>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-* {box-sizing: border-box;}
-
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-.topnav .login-container {
-  float: right;
-}
-
-.topnav .login-container a {
-  float: right;
-  padding: 6px 10px;
-  margin-top: 8px;
-  margin-right: 16px;
-  background-color: #555;
-  color: white;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-.topnav .login-container a:hover {
-  background-color: #2196F3;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav .login-container {
-    float: none;
-  }
-}
-</style>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/Home-001.css">
 </head>
 <body>
 
