4f0de58c8e86fe719c485f52576f6142dc45b1e6
kepsgurih <kepsgurih@pm.me>
2021-03-13 11:49:06 +0700

    Camp

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		require_once APPPATH.'../vendor/autoload.php';
+//Step 1: Enter you google account credentials
+
+		$g_client = new Google_Client();
+
+		$g_client->setClientId("362206884080-qpfsespd4ft2ivdi5uk6l3k4nuuper8k.apps.googleusercontent.com");
+		$g_client->setClientSecret("0MfVs4IRnCC7w-X97Pd0C_3o");
+		$g_client->setRedirectUri("http://localhost/webapp");
+		$g_client->setScopes("email");
+		$g_client->setScopes(array('profile','email'));
+
+//Step 2 : Create the url
+		$auth_url = $g_client->createAuthUrl();
+		$data['auth_url'] =$auth_url;
+// HTML
+		$this->load->view('login',$data);
+
+//Step 3 : Get the authorization  code
+		$code = isset($_GET['code']) ? $_GET['code'] : NULL;
+
+//Step 4: Get access token
+		if(isset($code)) {
+
+			try {
+
+				$token = $g_client->fetchAccessTokenWithAuthCode($code);
+				$g_client->setAccessToken($token);
+
+			}catch (Exception $e){
+				echo $e->getMessage();
+			}
+
+
+
+
+			try {
+				$pay_load = $g_client->verifyIdToken();
+
+
+			}catch (Exception $e) {
+				echo $e->getMessage();
+			}
+
+		} else{
+			$pay_load = null;
+		}
+
+		if(isset($pay_load)){
+			$this->session->set_userdata($pay_load);
+			echo var_dump($pay_load);
+			echo $pay_load['email'];
+
+
+		}
+		
+	}
+	public function in(){
+		$data['userdata'] = $_SESSION;
+		$this->load->view('welcome_message.php',$data);
+	}
+}
